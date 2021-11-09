59b24c22340501b07b9484bd67d53969f053e384
Shreesh Pandey <shvspandey@gmail.com>
2021-11-09 17:54:13 +0530

    Ability to Create a Queue a Stack of 56 -> 30 -> 70

---
+++ LinkedQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    class LinkedQueue
+    {
+        Node head = null;
+        internal void enQueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)//if the queue is empty
+            {
+                head = node;//initialize data entered as head.
+            }
+            else
+            {
+                Node temp = head;//it will assign a temporay variable as head.
+                while (temp.next != null)//till it finds an empty node
+                {
+                    temp = temp.next;//data will be assigned to it.
+                }
+                temp.next = node;//the next value will be assigned as tail.
+            }
+            Console.WriteLine(node.data);//will print the queue.
+        }
+        internal void Display()//method used to display the Stack.
+        {
+            Node temp = this.head;//inintialize the pointer to head.
+            if (temp == null)//if the head is empty then the stack is empty.
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            while (temp != null)//will check till the empty node is found.
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,14 +6,11 @@ namespace StackQueue
     {
         static void Main(string[] args)
         {
-            LinkedStack linkedStack = new LinkedStack();//object of the LinkedStack Class.
-            linkedStack.pushElement(70);//this will go in the last.
-            linkedStack.pushElement(30);
-            linkedStack.pushElement(56);//this will go in the top.
-            linkedStack.peakOperation();//this will deisplay the top elemnent without deleting it.
-            linkedStack.popOperation();
-            linkedStack.Display();//this will display.
-            //linkedStack.isEmpty();//used to display both the peak and pop.
+            LinkedQueue linkedQueue = new LinkedQueue();//object of LinkedQueue created.
+            linkedQueue.enQueue(56);
+            linkedQueue.enQueue(30);
+            linkedQueue.enQueue(70);
+            linkedQueue.Display();
         }
     }
 }
